cac8c19d21017a03e525efe59e3d2777fb61291a
Oluseye Fadiran <abclogics@gmail.com>
2026-04-25 19:09:03 -0400

    Build Next.js personal website

--- OneDrive - University of the Cumberlands/Documents/New project/mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentType } from "react";
-
-type MDXComponents = Record<string, ComponentType<Record<string, unknown>> | string>;
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components
-  };
-}
